ca5d1a466d203986e722bd994c800a042a3584ce
Sloane <sloane.simmons@toyotaconnected.com>
2024-10-21 11:29:47 -0500

    Finalize tests
    - Add more tests
    - Make llama_state / small_model module scope (so don't need to reload for each test)
      - Setting env var in `.env` file

--- tests/test_llama_cache.py
+++ tests/test_llama_cache.py
@@ -7,7 +7,9 @@ from llama_cpp.llama import Llama, LlamaState
 from llama_cpp.llama_cache import LlamaStaticDiskCache, StateReloadError
 
 
-@pytest.fixture
+# Have to be careful to reset to good state when testing, but don't want to
+# recreate model each time.
+@pytest.fixture(scope="module")
 def small_model():
     model_filename = os.getenv("LLAMA_TEST_MODEL")
     if not model_filename:
@@ -64,9 +66,11 @@ The current state of the ship is:
     return test_model
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def llama_state(small_model) -> LlamaState:
     state = small_model.save_state()
+    # Clear scores so that can test reloading from cache without them.
+    state.scores = None
     return state
 
 
@@ -130,6 +134,9 @@ def test_disk_cache_e2e(small_model: Llama):
             state = cache[key]
             assert ~(state.input_ids == 0).all()
             assert state is not None
+            assert (
+                state.scores is None
+            ), "Logits should not be stored when save_logits=False and model doesn't require them."
 
             small_model.reset()
             small_model.input_ids[:] = 0
@@ -147,3 +154,88 @@ def test_disk_cache_e2e(small_model: Llama):
             last_logits2 = small_model.scores[small_model.n_tokens - 1, :]
 
             assert (last_logits == last_logits2).all()
+
+
+def test_cache_save_reload_scores_when_needed(
+    small_model: Llama,
+):
+    """
+    When model requires it, can reload from state with scores.
+    """
+    test_prompt = "this is a test prompt"
+    with tempfile.TemporaryDirectory() as cache_dir:
+        cache = LlamaStaticDiskCache.build_cache(
+            cache_dir=cache_dir,
+            prompts=[test_prompt],
+            model=small_model,
+            capacity_bytes=2 << 30,
+            add_bos=True,
+            seed=1234,
+            save_logits=True,
+        )
+
+        llama_state = small_model.save_state()
+        cur_scores = llama_state.scores.copy()
+        assert ~(cur_scores == 0.0).all()
+
+        try:
+            small_model.context_params.logits_all = True
+            state_from_cache = cache[
+                tuple(llama_state.input_ids[: llama_state.n_tokens].tolist())
+            ]
+            assert state_from_cache.scores is not None, "Scores should be saved."
+            LlamaStaticDiskCache.reload_from_cache_state(small_model, state_from_cache)
+            # Do I have to limit these to n_tokens?
+            assert (state_from_cache.input_ids == llama_state.input_ids).all()
+            assert (
+                cur_scores == small_model.scores[: small_model.n_tokens]
+            ).all(), "Reloaded scores should match"
+        finally:
+            small_model.scores[:] = 0.0
+            small_model.context_params.logits_all = False
+            small_model.reset()
+
+
+def test_cache_reload_errors_when_requires_scores_and_state_doesnt_have_it(
+    small_model: Llama, llama_state: LlamaState
+):
+    """
+    If model requires logits for sampling and state doesn't have it, should raise error.
+    """
+    old_state_scores = (
+        llama_state.scores.copy()
+        if llama_state.scores is not None
+        else llama_state.scores
+    )
+    try:
+        small_model.context_params.logits_all = True
+        llama_state.scores = None
+
+        with pytest.raises(StateReloadError):
+            LlamaStaticDiskCache.reload_from_cache_state(small_model, llama_state)
+    finally:
+        small_model.context_params.logits_all = False
+        llama_state.scores = old_state_scores
+
+
+# pylint: disable=invalid-name
+def test_cache_errors_when_save_logits_False_but_model_requires(small_model: Llama):
+    """
+    If model requires logits but save_logits is False, should raise error.
+    """
+
+    try:
+        small_model.context_params.logits_all = True
+        with pytest.raises(ValueError):
+            with tempfile.TemporaryDirectory() as cache_dir:
+                LlamaStaticDiskCache.build_cache(
+                    cache_dir=cache_dir,
+                    prompts=["this is a test prompt"],
+                    model=small_model,
+                    capacity_bytes=2 << 30,
+                    add_bos=True,
+                    seed=1234,
+                    save_logits=False,
+                )
+    finally:
+        small_model.context_params.logits_all = False
